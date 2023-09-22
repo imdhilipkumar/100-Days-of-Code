1520b4fe2900d20d40e0a6445f5ea55f120c9752
Dhilipkumar <dondhilip0007@gmail.com>
2023-09-22 14:27:00 +0530

    First Commit

---
+++ Day 26/nano alphabet.py
@@ -0,0 +1,23 @@
+student_dict = {
+    "student": ["Angela", "James", "Lily"], 
+    "score": [56, 76, 98]
+}
+
+# TO READ THE CSV FILE
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+
+#TODO 1. Create a dictionary in this format:
+#syntax : {key:value for (key, value) in dictionary.items()}
+# syntax for to read row
+#syntax: (new_key : new_value  for (index, row) in dictionary.iterrow()}
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+#print(phonetic_dict)
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
+word = input("Enter a word : ").upper()
+output_list = [phonetic_dict[letter] for letter in word]
+print(output_list)
